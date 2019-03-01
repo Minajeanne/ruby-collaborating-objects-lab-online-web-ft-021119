f36c00d1be0d12bf930055558d9aef31cc06c721
Alicia Santiago <albrown07@mac.com>
2019-03-01 19:08:09 +0000

    Done.

---
+++ lib/mp3_importer.rb
@@ -0,0 +1,23 @@
+class MP3Importer
+  attr_accessor :path, :files
+
+  def initialize(path)
+    @path = path
+    # @files = Dir.new(path).grep(/.*\.mp3/)
+  end
+
+  def files
+     files = []
+     Dir.new(self.path).each do |file|
+       files << file if file.length > 4
+     end
+     files
+   end
+
+  def import
+    self.files.each do |filename|
+      Song.new_by_filename(filename)
+    end
+  end
+
+end
---
+++ lib/song.rb
@@ -0,0 +1,42 @@
+require 'pry'
+class Song
+  attr_accessor :name, :artist
+
+  def initialize(name)
+    @name = name
+  end
+
+  # def artist_name=(name)
+  #   self.artist = Artist.find_or_create_by_name(name)
+  # end
+
+  def self.new_by_filename(filename)
+    array = filename.split(/[-,]/)
+    @artist = array[0].strip
+    @song_name = array[1].strip
+    @song = Song.new(@song_name)
+    @artist = Artist.find_or_create_by_name(@artist)
+    @song.artist = @artist
+    # binding.pry
+    @song
+  end
+
+  # song = self.new
+  # song_info = file.chomp(".mp3").split(" - ")
+  # song = Song.new(song_info[1])
+  # song.artist_name = song_info[0]
+
+# def self.new_by_filename(file)
+#   artist_name, song_name, extra = filename.split(" - ")
+#    song = self.new(song_name)
+#    artist = Artist.find_or_create_by_name(artist_name)
+#    artist.add_song(song)
+#  end
+  # def self.new_by_filename(filename)
+  #   song = self.new
+  #   song_name = filename.split(" - ")[1]
+  #   artist_name = filename.split(" - ")[0]
+  #   song.artist = Artist.find_or_create_by_name(name)
+  #   song
+  # end
+end
